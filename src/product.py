4230e0696682da4b632710a0e7e268caaec0034d
polina dorozhkina <pollyyn0754@gmail.com>
2026-01-29 22:21:52 +0300

    добавлена новая функциональность в модуль `product`, внесены корректировки в модуле тестирования

--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+from typing import Optional, Self, Union
+
+
 class Product:
     """Класс для представления товара"""
 
@@ -6,13 +9,61 @@ class Product:
     price: float  # цена
     quantity: int  # количество в наличии
 
-    def __init__(self, name: str, description:str, price: float, quantity: int =0) -> None:
+    def __init__(self, name: str, description: str, price: float, quantity: int = 0) -> None:
         """Метод для инициализации экземпляра класса"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     def __repr__(self) -> str:
         """Метод для отображения информации о товарах"""
         return f"Product(name='{self.name}', price={self.price}, quantity={self.quantity})"
+
+    @classmethod
+    def new_product(cls, product_data: dict, products_list: Optional[list] = None) -> Union[Self, list]:
+        """Класс-метод, который будет принимать на вход параметры товара в словаре
+        и возвращать созданный объект или обновляет существующий в списке current_products"""
+
+        # Извлекаем параметры из словаря
+        name = product_data.get("name", "")
+        description = product_data.get("description", "")
+        price = round(product_data.get("price", 0.0))
+        quantity = int(product_data.get("quantity", 0))
+
+        if products_list:
+            for prod in products_list:
+                if prod["name"] == name:
+                    # Складываем количество
+                    prod["quantity"] += quantity
+                    # Выбираем максимальную цену
+                    prod["price"] = max(prod["price"], price)
+                    return products_list
+
+        # Если товар не найден в списке или список пуст, создаем новый
+        return cls(name, description, price, quantity)
+
+    @property  # type: ignore[no-redef]
+    def price(self) -> float:
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер для цены с проверками"""
+        # проверка введения некорректной цены
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        # если цена товара понижается, пользователь вручную дает согласие через ввод
+        # 'y' (значит yes) или 'n' (значит no)
+        if new_price < self.__price:
+            choice_price = input(f"Цена товара {self.name} понижается. Вы уверены? (y/n): ").lower()
+            if choice_price == "y":
+                self.__price = new_price
+                print("Цена успешно понижена")
+            else:
+                print("Действие отменено")
+        else:
+            self.__price = new_price
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,6 @@
 # mypy: disable-error-code="no-untyped-def"
+from src.category import Category
+from src.product import Product
 
 
 def test_product_init(sample_product):
@@ -17,3 +19,54 @@ def test_product_repr(sample_product):
 def test_product_default_quantity(product_no_quantity):
     """Тест значения количества по умолчанию"""
     assert product_no_quantity.quantity == 0
+
+
+def test_price_setter_increase(sample_product):
+    """Тест повышения цены (без подтверждения)"""
+    sample_product.price = 100000.0
+    assert sample_product.price == 100000.0
+
+
+def test_price_setter_negative(sample_product, capsys):
+    """Тест попытки установить некорректную цену"""
+    sample_product.price = -10000
+    captured = capsys.readouterr()
+    assert "Цена не должна быть нулевая или отрицательная" in captured.out
+    assert sample_product.price == 95000.0
+
+
+def test_price_setter_decrease_confirm(sample_product, monkeypatch):
+    """Тест снижения цены с подтверждением 'y'"""
+    monkeypatch.setattr("builtins.input", lambda _: "y")
+    sample_product.price = 80000.0
+    assert sample_product.price == 80000.0
+
+
+def test_price_setter_decrease_cancel(sample_product, monkeypatch):
+    """Тест отмены снижения цены 'n'"""
+    monkeypatch.setattr("builtins.input", lambda _: "n")
+    sample_product.price = 80000.0
+    assert sample_product.price == 95000.0
+
+
+def test_new_product_creation():
+    """Тест создания нового продукта через classmethod"""
+    data = {"name": "Samsung", "description": "Android", "price": 80000.0, "quantity": 10}
+    new_obj = Product.new_product(data)
+    assert isinstance(new_obj, Product)
+    assert new_obj.name == "Samsung"
+
+
+def test_new_product_update_existing():
+    """Тест обновления существующего продукта (сложение количества и выбор макс. цены)"""
+    data = {"name": "Samsung Galaxy S23", "description": "128gb", "price": 120000.0, "quantity": 5}
+    new_data = [
+        {"name": "Samsung Galaxy S23", "description": "256GB, Серый цвет, 200MP камера",
+         "price": 180000.0, "quantity": 5}
+    ]
+    updated_product = Product.new_product(data, new_data)
+
+    assert updated_product == [
+        {"name": "Samsung Galaxy S23", "description": "256GB, Серый цвет, 200MP камера",
+         "price": 180000.0, "quantity": 10}
+    ]
