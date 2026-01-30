9ffe9d4420087d7b910a63580a937d4a454b8cff
polina dorozhkina <pollyyn0754@gmail.com>
2026-01-31 01:41:11 +0300

    добавлен класс `product_iterator`

---
+++ src/product_iterator.py
@@ -0,0 +1,18 @@
+
+
+class ProductIterator:
+    def __init__(self, category_obj):
+        self.category = category_obj
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.category.products_in_list):
+            product = self.category.products_in_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
